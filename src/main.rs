c22d15693c2d196e4bc3f01566def1a33e4da96d
gentleli <v_ljtli@tencent.com>
2023-08-16 23:48:45 +0800

    two

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 mod guess_game;
 mod comm_concepts;
+mod control_flow;
+mod ownership;
+mod ref_and_borrow;
 
 fn main() {
     println!("Hello, world!");
     // guess_game::guess_game_random03();
-    comm_concepts::data_type();
+    // comm_concepts::data_type();
+    // control_flow::test01();
+    // ownership::ownership01();
+    ref_and_borrow::references_and_Bborrowing01();
 }
 
 
